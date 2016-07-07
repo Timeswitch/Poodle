184578c09a0320d7170eab9619e398dbebe8213d
jan friesen <jan-friesen@web.de>
2016-07-07 21:20:21 +0200

    slot hinzufügen hinzugefügt

---
+++ src/main/java/de/hsos/kbse/view/CreateSlotView.java
@@ -0,0 +1,71 @@
+package de.hsos.kbse.view;
+
+import com.vaadin.cdi.CDIView;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.shared.ui.datefield.Resolution;
+import com.vaadin.ui.*;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Created by jan on 07.07.2016.
+ */
+
+@CDIView("createSlot")
+@RolesAllowed({"PROFESSOR"})
+public class CreateSlotView extends CustomComponent implements View{
+
+    private Button abortButton;
+    private Button addButton;
+    private InlineDateField date;
+
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setSpacing(true);
+        verticalLayout.setMargin(true);
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.setSpacing(true);
+
+        Label header = new Label("Slot hinzufügen");
+        header.setStyleName("h1");
+
+        abortButton = new Button("Abbrechen");
+        abortButton.setStyleName("primary");
+        abortButton.addClickListener((Button.ClickListener)event1 -> this.onAbortClick());
+
+        addButton = new Button("Hinzufügen");
+        addButton.setStyleName("primary");
+        addButton.addClickListener((Button.ClickListener)event2 -> this.onAddClick());
+
+        date = new InlineDateField();
+        date.setValue(new Date());
+        date.setImmediate(true);
+        date.setTimeZone(TimeZone.getTimeZone("UTC"));
+        date.setLocale(Locale.GERMAN);
+        date.setResolution(Resolution.MINUTE);
+
+        horizontalLayout.addComponent(addButton);
+        horizontalLayout.addComponent(abortButton);
+
+        verticalLayout.addComponent(header);
+        verticalLayout.addComponent(date);
+        verticalLayout.addComponent(horizontalLayout);
+
+        setCompositionRoot(verticalLayout);
+    }
+
+    private void onAbortClick(){
+
+    }
+
+    private void onAddClick(){
+
+    }
+}
