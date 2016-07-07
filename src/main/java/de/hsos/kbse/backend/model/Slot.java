1992fbe76f917b89a03cf7d90e4d8b82b36147c5
Michael Kroll <michael.kroll.94@googlemail.com>
2016-07-07 11:25:12 +0200

    fehlende felder für slot

--- src/main/java/de/hsos/kbse/backend/model/Slot.java
+++ src/main/java/de/hsos/kbse/backend/model/Slot.java
@@ -1,20 +1,45 @@
 package de.hsos.kbse.backend.model;
 
+import javax.enterprise.inject.Vetoed;
 import javax.persistence.Embeddable;
+import javax.persistence.OneToOne;
+import java.sql.Date;
+import java.sql.Time;
 
 /**
  * Created by jan on 26.06.2016.
  */
 @Embeddable
+@Vetoed
 public class Slot {
 
-    String test;
+    @OneToOne
+    Student student;
 
-    public String getTest() {
-        return test;
+    Date date;
+    Time time;
+
+    public Student getStudent() {
+        return student;
     }
 
-    public void setTest(String test) {
-        this.test = test;
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
     }
 }
